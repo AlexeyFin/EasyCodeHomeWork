16ef658e73df2c53d27ed203f92b200a47c36669
Alex Fin <alexey.finohenov@gmail.com>
2018-01-23 16:27:01 +0200

    hw1

---
+++ hw1/hw1.js
@@ -0,0 +1,139 @@
+//Переменные. Задачи (var)
+
+//1
+
+// var price;
+// var MAX_NUMBER;
+// var USER_NAME;
+// var userInfo;
+
+
+//2
+// console.log(test);
+// var test = 'string';
+// скачет что переменная test undefined
+
+
+// var x = 'string';
+// var x = 'srting №2';
+// console.log(x);
+
+//выведет строку 'srting №2'
+
+//(let & const)
+//3
+
+// console.log(test);
+// let test = 'string';
+//скачет что переменная test не задекларированна
+
+
+// let someVariable = 15;
+// let someVariable = 10;
+//
+// ошибка, переменная someVariable объявлена дважды
+
+
+
+//Строки. Задачи
+
+
+// let string = 'some test string';
+// let result;
+// //1
+//
+// result = string[0];//Первая буква задание 1
+// result = string[string.length - 1];// последняя буква задание 1
+// //Первая и последняя в верхнем регистре задание 2
+// result = string[0].toUpperCase() + string.slice(1, string.length - 1) + string[string.length - 1].toUpperCase();
+//
+// result = string.indexOf('string');// положение слова в строке задание 3
+// result = string.lastIndexOf(' ');// положение второго пробела задание 4
+// result = string.slice(4, 9);// задание 5
+// result = string.slice(4, 10);//задание 6
+// result = string.slice(0, string.length -6);//задание 7
+//console.log(result);
+
+// задание 8
+// let a = 20;
+// let b = 16;
+//
+// string = a  + '' + b;
+// console.log(string);
+// console.log(typeof(string));
+
+
+
+
+// Числа. задачи
+//
+// let result;
+//
+// result = (Math.PI).toFixed(2);// task 1
+//
+// result = Math.max(15, 11, 16, 12, 51, 12, 13, 51);// task 2
+//
+// result = (Math.random()).toFixed(2);//task 3a
+//
+// result = Math.round(Math.random() * 100);//task 3b
+//
+// result = (Math.ceil((0.6 + 0.7)*10))/10;//task 4
+//
+// result = parseInt('100$');//task 5
+//
+// console.log(result);
+
+
+
+//Объект Задачи
+
+
+// let obj = {
+//     product: 'iphone'
+// };
+// obj.price = 1000;
+// obj.currency = 'dollar';
+// obj.detalis = {
+//     model: 'IPhone X',
+//     color: 'Black'
+// };
+//
+//
+//
+// console.log(obj);
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
