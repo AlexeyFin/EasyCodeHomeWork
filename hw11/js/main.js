b745195a798748314715cd54dd336e2899535c4e
Alex Fin <alexey.finohenov@gmail.com>
2018-03-15 18:03:40 +0200

    11.2

--- hw11/js/main.js
+++ hw11/js/main.js
@@ -33,24 +33,51 @@ fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
 
 
 
-// Task number 5
+// Task number 5.1
 
 
-Promise.all([
-    fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'}),
-    fetch('https://jsonplaceholder.typicode.com/users',{method: 'GET'})
-])
-    .then(arr => {
-        let data = [];
-        arr.forEach(item => data.push(item.json()));
-        return data
+// Promise.all([
+//     fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'}),
+//     fetch('https://jsonplaceholder.typicode.com/users',{method: 'GET'})
+// ])
+//     .then(arr => {
+//         let data = [];
+//         arr.forEach(item => data.push(item.json()));
+//         return data
+//     })
+//     .then(data => {
+//         data.forEach(item => {
+//             item.then(json => {
+///Вывод количества постов/юзеров равному длине массива
+//                 console.log(json.length)
+//             })
+//         })
+//     })
+//     .catch(err => console.log(err));
+
+//Task number 5.2
+let totalData = {
+    'posts': {},
+    'users': {}
+};
+
+let postsFetch = fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
+    .then(res => res.json());
+let usersFetch = fetch('https://jsonplaceholder.typicode.com/users',{method: 'GET'})
+    .then(res => res.json());
+
+
+Promise.all([postsFetch, usersFetch])
+    .then(data => {
+        totalData['posts'] = data[0];
+        totalData['users'] = data[1];
+
+        return totalData
     })
     .then(data => {
-        data.forEach(item => {
-            item.then(json => {
-                console.log(json.length)
-            })
-        })
+        console.log(`Количество постов: ${data.posts.length}`);
+        console.log(`Количество юзеров: ${data.users.length}`);
     })
     .catch(err => console.log(err));
 
+//Как лучше было это сделать?
