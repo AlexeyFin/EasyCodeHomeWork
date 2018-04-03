6e1f39a68304614c79990dca7769d512d2d214ff
Alex Fin <alexey.finohenov@gmail.com>
2018-04-03 23:57:15 +0300

    hw15, alot of modules or how to get crazy by using ES6

---
+++ hw15/js/tasks.js
@@ -0,0 +1,70 @@
+// Init Id module
+const id = Id;
+
+const Tasks = (function () {
+    
+    let tasks = [];
+    let instance;
+    
+    const getTasks = function () {
+        return tasks;
+    };
+    
+    const setTasks = function (array) {
+        tasks = array;
+        return tasks;
+    };
+    
+    const addTask = async function (task) {
+        task.id = id.generate();
+        await tasks.unshift(task);
+        return task;
+    };
+
+    const removeTask = async function (id) {
+        tasks = await tasks.filter(task => task.id !== id);
+        return tasks;
+    };
+
+    const removeAll = async function () {
+        tasks = [];
+    };
+
+    const editTask = async function (data) {
+        for( let i = 0; i < tasks.length; i++){
+            if (data.id === tasks[i].id){
+                await tasks.splice(i, 1, data);
+                break
+            }
+        }
+
+        return tasks
+    };
+
+    const searchItems = async function (text) {
+        let data = tasks.filter(task => {
+            return (task.text.indexOf(text) + 1)
+        });
+
+        return data
+    };
+
+    const createInstance = function () {
+        return {
+            getTasks,
+            setTasks,
+            addTask,
+            removeTask,
+            removeAll,
+            editTask,
+            searchItems,
+        }
+    };
+
+    return {
+        getInstance: function () {
+            return instance || (instance = createInstance());
+        }
+    }
+    
+}());
